3df87b537cf8d456574b2b82fe85ee0abf59f8a0
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 17:27:25 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,17 +1,27 @@
 
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
+
+// Register Events Liststener
 playerManager.addEventListener(
 	cast.framework.events.EventType.MEDIA_STATUS, (event) => {
 		hanldeEvent(event)
 	}
 );
+
+// Register Messages Liststener
+playerManager.setMessageInterceptor(
+	cast.framework.messages.MessageType.LOAD, loadRequestData => {
+		handleMessage(loadRequestData);
+	}
+);
+
 context.start(options);
 
 function hanldeEvent(event) {
 	console.log(event);
 }
 
-function handleMessage(message) {
-	console.log(message);
+function handleMessage(loadRequestData) {
+	console.log(loadRequestData);
 }
