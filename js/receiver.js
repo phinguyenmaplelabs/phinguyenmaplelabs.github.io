a9eca57fed25370844ca20be39ae4692624e4042
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 16:10:05 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,2 @@
 const context = cast.framework.CastReceiverContext.getInstance();
-
-let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
-
 context.start();
