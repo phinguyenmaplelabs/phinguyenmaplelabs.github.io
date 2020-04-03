111cccfb7ae6c7be365677b5fb4aed5cf1eaa38e
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 16:47:34 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,6 @@
 
 
-
+console.error('Browser not supported!');
 const context = cast.framework.CastReceiverContext.getInstance();
 const options = new cast.framework.CastReceiverOptions();
 options.maxInactivity = 60;
