09bdf3fc26835a483c23b53b231ba03a38deca4c
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 19:52:29 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -2,6 +2,8 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = cast.framework.CastReceiverContext.getInstance().getPlayerManager();
 
+console.log("Register Events Liststener");
+
 // Register Events Liststener
 playerManager.addEventListener(
 	cast.framework.events.EventType.MEDIA_STATUS, (event) => {
