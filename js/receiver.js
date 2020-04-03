3c7524a1d46b2f1c9c0a59ccda10cd8133c87f3c
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 17:22:56 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,17 @@
 
-
-console.error('Browser not supported!');
 const context = cast.framework.CastReceiverContext.getInstance();
-const options = new cast.framework.CastReceiverOptions();
+const playerManager = context.getPlayerManager();
+playerManager.addEventListener(
+	cast.framework.events.EventType.MEDIA_STATUS, (event) => {
+		hanldeEvent(event)
+	}
+);
 context.start(options);
+
+function hanldeEvent(event) {
+	console.log(event);
+}
+
+function handleMessage(message) {
+	console.log(message);
+}
