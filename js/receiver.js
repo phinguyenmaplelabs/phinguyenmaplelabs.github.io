652a4d45adedabed02ce7c912a10030d27bdfcde
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-03 16:39:55 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,7 @@
+
+
+
 const context = cast.framework.CastReceiverContext.getInstance();
-
-let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("http://maplelabs.co/wp-content/uploads/2020/04/100x100-1.png")');
-
-context.start();
+const options = new cast.framework.CastReceiverOptions();
+options.maxInactivity = 60;
+context.start(options);
