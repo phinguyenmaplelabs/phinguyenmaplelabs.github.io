ead561230723cb03f56794879c0a6635ea844333
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-07 14:28:21 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -3,12 +3,8 @@ console.log("Hello world!");
 
 const CHANNEL = 'urn:x-cast:com.tvcast.screenmirror';
 const ctx = cast.framework.CastReceiverContext.getInstance();
-const options = new cast.framework.CastReceiverOptions();
-
-options.customNamespaces = Object.assign({});
-options.customNamespaces[CHANNEL] = cast.framework.system.MessageType.JSON;
 //receiving sender message
 ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
-	console.log(customEvent.data.msg);
+	console.log(customEvent);
 });
-ctx.start(options);
+ctx.start();
