a101e8cde7643d41e2df0848f20453f980ff79f7
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-09 11:11:47 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -7,23 +7,10 @@ function main() {
 	const ctx 			= cast.framework.CastReceiverContext.getInstance();
 	const playerManager = ctx.getPlayerManager();
 	const commands      = cast.framework.messages.Command;
-	var options 		= new cast.framework.CastReceiverOptions();
 	/*
 	* Register Player Command
 	*/
-	options.supportedCommands = commands.PAUSE | commands.STREAM_VOLUME | commands.STREAM_MUTE;
-	/*
-	* Playback Configs
-	*/
-	const playbackConfig = new cast.framework.PlaybackConfig();
-    playbackConfig.manifestRequestHandler = requestInfo => {
-          requestInfo.withCredentials = true;
-    };
-
-    playbackConfig.segmentRequestHandler = requestInfo => {
-          requestInfo.withCredentials = true;
-	};
-	options.playbackConfig = playbackConfig;
+	playerManager.setSupportedMediaCommands(commands.SEEK | commands.PAUSE | commands.STREAM_VOLUME | commands.STREAM_MUTE | commands.SKIP_AD);
 	/*
 	*	Handle Player
 	*/
@@ -51,12 +38,14 @@ function main() {
 			hideIframe();
 		}
 	});
-	ctx.start(options);
+	ctx.start();
 }
+
 function showIframe() {
 	document.getElementById("cast_player").style.visibility = 'hidden';
 	document.getElementById("browserIframe").style.visibility = 'visible';
 }
+
 function hideIframe() {
 	document.getElementById("cast_player").style.visibility = 'visible';
 	document.getElementById("browserIframe").style.visibility = 'hidden';
