1a7e32d5e3b855359aea0a4b2c2657f63c789006
Phi Nguyen <phinnguyen@maplelabs.co>
2020-04-10 17:48:54 +0700

    no message

--- js/receiver.js
+++ js/receiver.js
@@ -1,81 +1,56 @@
-/*
-Copyright 2019 Google LLC. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+main();
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
-This sample demonstrates how to build your own Receiver for use with Google
-Cast.
-*/
+function main() {
+	const CHANNEL 			= 'urn:x-cast:com.tvcast.screenmirror';
+	const ctx 				= cast.framework.CastReceiverContext.getInstance();
+	const playerManager 	= ctx.getPlayerManager();
+	const commands      	= cast.framework.messages.Command;
+	const playbackConfig 	= new cast.framework.PlaybackConfig();
+	var options 			= new cast.framework.CastReceiverOptions();
+	/*
+	*	Handle Player
+	*/
+	playerManager.setMessageInterceptor(cast.framework.messages.MessageType.LOAD, loadRequestData =>{
+		const error = new cast.framework.messages.ErrorData(
+                      cast.framework.messages.ErrorType.LOAD_FAILED);
+      if (!loadRequestData.media) {
+        error.reason = cast.framework.messages.ErrorReason.INVALID_PARAM;
+        return error;
+	  }
+	  hideIframe();
+      return loadRequestData;
+	});
 
-'use strict';
+	/*
+	* Handle WebBrowser
+	*/
+	ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
+		var js = customEvent.data;
+		if (js.type == 'iframe') {
+			showIframe();
+			// playerManager.stop();
+			document.getElementById('browserIframe').src = js.url;
+		}else if (js.type == 'close_browser') {
+			hideIframe();
+		}
+	});
+	/*
+	* Config playback
+	*/
+	playbackConfig.autoResumeDuration = 1;
+	options.playbackConfig 		= playbackConfig;
+	options.supportedCommands 	= commands.SEEK | commands.PAUSE | commands.STREAM_VOLUME | commands.STREAM_MUTE
+	ctx.start(options);
+}
 
-import { CastQueue } from './queuing.js';
+function showIframe() {
+	document.getElementById("cast_player").style.visibility = 'hidden';
+	document.getElementById("browserIframe").style.visibility = 'visible';
+}
 
-const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
-
-// Listen and log all Core Events.
-playerManager.addEventListener(cast.framework.events.category.CORE,
-  event => {
-    console.log("Core event: " + event.type);
-    console.log(event);
-  });
-
-playerManager.addEventListener(
-  cast.framework.events.EventType.MEDIA_STATUS, (event) => {
-    console.log("MEDIA_STATUS event: " + event.type);
-    console.log(event);
-});
-
-// Intercept the LOAD request to be able to read in a contentId and get data.
-playerManager.setMessageInterceptor(
-  cast.framework.messages.MessageType.LOAD, loadRequestData => {
-    return loadRequestData;
-  });
-
-const playbackConfig = new cast.framework.PlaybackConfig();
-
-// Set the player to start playback as soon as there are five seconds of
-// media content buffered. Default is 10.
-playbackConfig.autoResumeDuration = 0;
-
-// Set the available buttons in the UI controls.
-const controls = cast.framework.ui.Controls.getInstance();
-controls.clearDefaultSlotAssignments();
-
-// Assign buttons to control slots.
-controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_1,
-  cast.framework.ui.ControlsButton.QUEUE_PREV
-)
-controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_2,
-  cast.framework.ui.ControlsButton.CAPTIONS
-)
-controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_3,
-  cast.framework.ui.ControlsButton.SEEK_FORWARD_15
-)
-controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_4,
-  cast.framework.ui.ControlsButton.QUEUE_NEXT
-)
-
-context.start({
-  queue: new CastQueue(),
-  playbackConfig: playbackConfig,
-  supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
-                      cast.framework.messages.Command.QUEUE_PREV |
-                      cast.framework.messages.Command.QUEUE_NEXT
-});
+function hideIframe() {
+	document.getElementById("cast_player").style.visibility = 'visible';
+	document.getElementById("browserIframe").style.visibility = 'hidden';
+}
